de5e739b15c31bcb87b89ff2e9ff3c2f1c6b5665
Constantin Saguin <wolfthemes14@gmail.com>
2026-07-02 11:35:34 +0200

    Prepare multisite config

--- config/application.php
+++ config/application.php
@@ -178,6 +178,17 @@ if (file_exists($env_config)) {
     require_once $env_config;
 }
 
+/* Multisite */
+/* Config::define('WP_ALLOW_MULTISITE', true); */
+/* Config::define('MULTISITE', true); */
+/* Config::define('SUBDOMAIN_INSTALL', false); // Set to true if using subdomains */
+/* Config::define('DOMAIN_CURRENT_SITE', env('DOMAIN_CURRENT_SITE')); */
+/* Config::define('PATH_CURRENT_SITE', env('PATH_CURRENT_SITE') ?: '/'); */
+/* Config::define('SITE_ID_CURRENT_SITE', env('SITE_ID_CURRENT_SITE') ?: 1); */
+/* Config::define('BLOG_ID_CURRENT_SITE', env('BLOG_ID_CURRENT_SITE') ?: 1); */
+/**/
+/* Config::define('WP_MEMORY_LIMIT', env('WP_MEMORY_LIMIT') ?: '2G'); */
+
 Config::apply();
 
 /**
